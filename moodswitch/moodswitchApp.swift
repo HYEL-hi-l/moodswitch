0213576a3dd967cc9d9dd10e52b1c5d1cd919421
Samuel Richard <sam@hyel.app>
2024-10-02 21:54:35 -0700

    Initial Commit

---
+++ moodswitch/moodswitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodswitchApp.swift
+//  moodswitch
+//
+//  Created by Sam Richard on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct moodswitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
